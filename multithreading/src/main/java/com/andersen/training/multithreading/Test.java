18121781820ce5ecf652014d425e2ddc9529bcfb
kets911@gmail.com <123qwerty>
2019-06-07 17:48:58 +0300

    Changed Test solution

--- multithreading/src/main/java/com/andersen/training/multithreading/Test.java
+++ multithreading/src/main/java/com/andersen/training/multithreading/Test.java
@@ -1,7 +1,9 @@
 package com.andersen.training.multithreading;
 
-import java.util.*;
-import java.util.logging.Level;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
 /**
@@ -13,39 +15,34 @@ import java.util.logging.Logger;
  */
 public class Test {
     private static Logger logger = Logger.getLogger(Test.class.getName());
-    private static volatile int threadCount;
-    private static List<Thread> threads = new ArrayList<>();
-    private static Thread currentThread;
+    private static volatile int count = -1;
 
     public static void main(String[] args) throws TestException, InterruptedException {
         Set<Double> res = new HashSet<>();
-        currentThread = Thread.currentThread();
+        List<Thread> threads = new ArrayList<>();
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < TestConsts.N; i++) {
-            if(threadCount == TestConsts.MAX_THREADS) {
-                try {
-                    res.wait();
-                } catch (InterruptedException e) {
-                    logger.log(Level.WARNING, e.getMessage(), e);
-                    throw new InterruptedException(e.getMessage());
-                }
-            }
-            Executor task = new Executor(i);
-            Thread thread = new Thread(() -> {
-                try {
-                    synchronized (res) {
-                        res.addAll(task.call());
-                        threadCount++;
-                        res.notify();
+
+        for (int i = 0; i < TestConsts.MAX_THREADS; i++) {
+            Thread exThread = new Thread(() -> {
+                while (count < TestConsts.N) {
+                    try {
+                        synchronized (res) {
+                            count++;
+                        }
+                        if (count < TestConsts.N) {
+                            Set<Double> result = TestCalc.calculate(count);
+                            synchronized (res) {
+                                res.addAll(result);
+                            }
+                        }
+                    } catch (TestException e) {
+                        count = TestConsts.N;
                     }
-                } catch (Exception e) {
-                    logger.log(Level.WARNING, e.getMessage(), e);
-                    Test.interrupt();
                 }
             });
-            thread.start();
-            threads.add(thread);
+            exThread.start();
+            threads.add(exThread);
         }
         for (Thread thread : threads) {
             thread.join();
@@ -54,10 +51,6 @@ public class Test {
         System.out.println("take time: " + takeTime);
 
         System.out.println(res);
-    }
-
-    public static void interrupt() {
-        currentThread.interrupt();
     }
 
 }
