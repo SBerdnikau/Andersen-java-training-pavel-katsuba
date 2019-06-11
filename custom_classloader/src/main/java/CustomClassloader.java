6f267338a43b641f2ed25acb60807dbd433fbe88
kets911@gmail.com <123qwerty>
2019-06-11 14:53:39 +0300

    Changed way to class loading

--- custom_classloader/src/main/java/CustomClassloader.java
+++ custom_classloader/src/main/java/CustomClassloader.java
@@ -9,25 +9,25 @@ public class CustomClassloader extends ClassLoader {
     }
 
     @Override
-    public synchronized Class loadClass(String name) throws ClassNotFoundException {
-
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
         try {
-            System.out.println("try loadClass (" + name + ") in System classLoader");
-            return super.loadClass(name);
-        } catch (Throwable e) {
-            try {
-                System.out.println("try loadClass (" + name + ") in Custom classLoader");
-                FileInputStream fin = new FileInputStream(name);
-                byte[] buff = new byte[(int) (new File(name).length())];
-                fin.read(buff);
-                fin.close();
-                Class<?> aClass = defineClass(getSimpleName(name), buff, 0, buff.length);
-                System.out.println("Class (" + name + ") was loaded in Custom classLoader");
-                return aClass;
-            } catch (IOException err) {
-                throw new ClassNotFoundException(e.getMessage(), err);
-            }
+            System.out.println("try loadClass (" + name + ") in Custom classLoader");
+            FileInputStream fin = new FileInputStream(name);
+            byte[] buff = new byte[(int) (new File(name).length())];
+            fin.read(buff);
+            fin.close();
+            Class<?> aClass = defineClass(getSimpleName(name), buff, 0, buff.length);
+            System.out.println("Class (" + name + ") was loaded in Custom classLoader");
+            return aClass;
+        } catch (IOException err) {
+            throw new ClassNotFoundException(err.getMessage(), err);
         }
+    }
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        System.out.println("try load class (" + name + ") in System classLoader");
+        return super.loadClass(name);
     }
 
     private String getSimpleName(String name) {
